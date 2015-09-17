642edd73403e77057af8e72ccf437960f7e167b8
varakin_ivan <varakin_ivan@yahoo.com>
2015-09-17 18:14:13 +0400

    3 занятие, 2 видеоурок

--- test/test_add_kontakt.py
+++ test/test_add_kontakt.py
@@ -3,15 +3,11 @@ from model.kontakt import Kontakt
 
 
 def test_add_kontakt(app):
-    app.session.login(username="admin", password="secret")
     app.group.fill_new_kontakt(Kontakt(first="qwer", middle="asdf", last="zxcv", phone="1234"))
-    app.session.logout()
 
 
 def test_add_empty_kontakt(app):
-    app.session.login(username="admin", password="secret")
     app.group.fill_new_kontakt(Kontakt(first="", middle="", last="", phone=""))
-    app.session.logout()
 
 
 
